0fdb4c96dfc4a0921b81a4e53d0daa41e9d3f2bc
valeyard <mrpontius@gmail.com>
2014-04-08 21:20:29 +0100

    basic word filter now works on post bodies
    excludes quote blocks and usernames, for now

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Trollface image must be at 'my_extension/images/trollface.jpg'.
   console.log("ERASSEDASE")
-  
+  var filter177 = {"\\bdaniel bryan\\b":"vanilla midget"};
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 var c = 0;
 
@@ -136,12 +136,12 @@ var cols = document.querySelectorAll('.smilie');
 
 $(".smilie_list").each(function(index, image) {
 	$this = $(image);
-	console.log(this);
+	//console.log(this);
 
 	var group = '<br><br><h3>Favourite Smilies</h3> <ul class="smilie_group">';
 	
 	var groupEnd = '</ul><br><br><br><br><br><br>'
-var ht = '<h3>Basic Smilies</h3> <ul class="smilie_group"> <li class="smilie"> <div class="text">:(</div> <img alt="" src="http://fi.somethingawful.com/images/smilies/frown.gif" title="frown"> <li class="smilie"> <div class="text">:)</div> <img alt="" src="http://fi.somethingawful.com/images/smilies/smile.gif" title="smile"> </ul>';
+  var ht = '<h3>Basic Smilies</h3> <ul class="smilie_group"> <li class="smilie"> <div class="text">:(</div> <img alt="" src="http://fi.somethingawful.com/images/smilies/frown.gif" title="frown"> <li class="smilie"> <div class="text">:)</div> <img alt="" src="http://fi.somethingawful.com/images/smilies/smile.gif" title="smile"> </ul>';
 	$(this).find(".inner").prepend(group + localStorage.favourite + groupEnd)
 		});
 
@@ -153,7 +153,7 @@ $(".smilie").each(function(index, image) {
 	this.setAttributeNode(att);;
 
   var att1=document.createAttribute("id");
-  console.log(this.getAttribute("id"));
+  //console.log(this.getAttribute("id"));
   if (this.getAttribute("id") != "favourite"){
     att1.value="basic";
     this.setAttributeNode(att1);;
@@ -162,35 +162,79 @@ $(".smilie").each(function(index, image) {
 
 $(".mainbodytextsmall").each(function(index, image) {
 	$this = $(image);
-	console.log(this.innerText);
+	//console.log(this.innerText);
 	 	var y = new RegExp("Hello, ([A-Za-z0-9]+)!")
-	 	console.log(y.test("Hello, Valeyard! Make all your dreams come true"));
+	 	//console.log(y.test("Hello, Valeyard! Make all your dreams come true"));
 	 	var g = y.exec(this.innerText);
 	 	
 	 	user = g[1];
 	 	localStorage.user = "";
 	 	localStorage.user=g[1];
-	 	console.log(localStorage.user);
+	 	//console.log(localStorage.user);
 	});
 
 $(".post").each(function(index, image) {
 	$this = $(image);
 	//var text = this.rows[0].innerText;
-	var text = $(this).find(" td.postbody")[0].innerText;
 
+var el = $(this).find(" td.postbody")[0],
+    child = el.firstChild,
+    texts = [];
+
+while (child) {
+    if (child.nodeType == 3) {
+        texts.push(child.data);
+    }
+    child = child.nextSibling;
+}
+
+var text = texts.join("");
+
+	//var text = $(this).find(" td.postbody")[0].innerText;
+console.log(text);
 	var author = $(this).find(" dt.author")[0].innerText;
     var x = new RegExp("(http|https)://(vine)\.(co)/v/[A-Za-z0-9]+");
-	console.log(author);
-	console.log(localStorage.user);
-	
- // $this = $(this.rows);
 
- 
+for(var j in filter177){
+
+  var h = new RegExp(j, 'gi');
+  console.log(j);
+  console.log(h);
+  var t = h.exec(text);
+  console.log(h.test(text))
+  console.log(t);
+  if (t != null){
+  $(this).find(" td.postbody")[0].innerText=$(this).find(" td.postbody")[0].innerText.replace(t, filter177[j])
+  //$(this).find(" td.postbody")[0].innerText = text;
+}
+}
+
+//  var r = new RegExp("\bdaniel bryan\b");
+  var re = /\bdaniel bryan\b/;
+  //console.log(re);
+//  var m;
+//  m = re.exec(text);
+
+//  if (m != null){
+//   console.log(m[0]);
+//   $(this).find(" td.postbody")[0].innerText=$(this).find(" td.postbody")[0].innerText.replace(re,"vanilla midget");
+//   console.log(text);
+//   //$(this).find(" td.postbody")[0].innerText = text;
+// }
+//text = text.replace("daniel bryan to vanilla midget","ASDASDASDASDWWWWW");
+//console.log($(this).find(" td.postbody")[0].innerText);
+      text = text.replace(/\bdaniel bryan\b/g, "vanillaasdasdas");
+      $(this).find(" td.postbody")[0].innerText.replace('daniel bryan is a va', "dsdfsdfsdfsdfs");
       //console.log(this.length);
       //console.log("owned async");
       text = text.replace(/\"/g,"&quot;");
+      text = text.replace(/d/g,"LOL");
       //console.log(text);
-    console.log(image);
+    //console.log(image);
+
+    //$(this).find(" td.postbody")[0].innerText.replace('/\bdaniel bryan\b/', "dsdfsdfsdfsdfs");
+
+
     if (author == localStorage.user){
     $(this).find(" ul.postbuttons").append('<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="manas" data-text="'+text+'" data-count="none" data-dnt="true">Tweet</a></li>');
 
@@ -198,25 +242,13 @@ $(".post").each(function(index, image) {
           if (x.test(text)){
         var url2 = x.exec(text);
        $(this).find(" td.postbody")[0].innerText.replace('/\b('+url2[0]+')\b/g', "dsdfsdfsdfsdfs");
-       //console.log($(this).find(" td.postbody")[0].innerText);
+
+              //console.log($(this).find(" td.postbody")[0].innerText);
        $(this).find(" td.postbody").append('<iframe class="vine-embed" src="'+url2[0]+'/embed/simple" width="600" height="600" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>');
+       $(this).find(" td.postbody")[0].innerText.replace("daniel bryan to vanilla midget","ASDASDASDASDWWWWW");
    }
  	}
   
- // $this = $(this.rows);
-
- 
-
-
-
-  //console.log(x.test("https://vine.co/v/h3Wm0PaxEpM someword"));
-  //var url = x.exec("https://vine.co/v/h3Wm0PaxEpM someword");
-  //console.log(url);
-      //console.log(this.length);
-      //console.log("owned async");
-
-      //console.log(text);
-     //console.log(image);
 
  });
 
