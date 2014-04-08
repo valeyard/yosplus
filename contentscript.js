1d213610149196ebc3971c3ea804dbc35231b26b
valeyard <mrpontius@gmail.com>
2014-04-08 02:32:08 +0100

    can now remove favourites

--- contentscript.js
+++ contentscript.js
@@ -31,9 +31,21 @@ else
 
 function handleDragStart(e) {
  // this.style.opacity = '0.4';  // this / e.target is the source node.
+
+ var cols = document.querySelectorAll('.smilie');
+[].forEach.call(cols, function(col) {
+  col.addEventListener('dragstart', handleDragStart, false);
+  col.addEventListener('dragenter', handleDragEnter, false)
+  col.addEventListener('dragover', handleDragOver, false);
+  col.addEventListener('dragleave', handleDragLeave, false);
+  col.addEventListener('drop', handleDrop, false);
+  col.addEventListener('dragend', handleDragEnd, false);
+});
+
   console.log("starting to drog");
   dragSrcEl = this;
-
+  source = this.getAttribute("id");
+  console.log(source);
   e.dataTransfer.effectAllowed = 'move';
   e.dataTransfer.setData('text/html', this.innerHTML);
 };
@@ -55,6 +67,7 @@ function handleDragEnter(e) {
 
 function handleDragLeave(e) {
   this.classList.remove('over');  // this / e.target is previous target element.
+
 }
 
 function handleDrop(e) {
@@ -69,10 +82,33 @@ function handleDrop(e) {
     // Set the source column's HTML to the HTML of the column we dropped on.
     //dragSrcEl.innerHTML.append(this.innerHTML);
     //console.log(this.innerHTML);
-    $('.standard').find('.smilie_group').append('<li class="smilie" draggable="true"> '+ dragSrcEl.innerHTML);
+
+  var att1=document.createAttribute("id");
+  
+
+  if (dragSrcEl.getAttribute("id") == "basic"){
+    att1.value="favourite";
+    dragSrcEl.setAttributeNode(att1);;
+        $('.standard').find('.smilie_group').append(dragSrcEl);
    // var htmlFav =  $('.standard').find('.smilie_group').innerHTML;
     console.log($('.standard').find('.smilie_group')[0].innerHTML);
     localStorage.favourite = $('.standard').find('.smilie_group')[0].innerHTML;
+  }
+
+  else{
+    console.log("already a favourite");
+    var title = dragSrcEl.innerText;
+    console.log(title);
+    $('.standard').find('.smilie_group').remove(dragSrcEl);
+    $(".smilie").each(function(index, image) {
+  $this = $(image);
+    if (title == image.innerText && this.getAttribute("id")=="favourite"){
+      console.log("this should print every time");
+      this.remove();
+      localStorage.favourite = $('.standard').find('.smilie_group')[0].innerHTML;
+    }
+  });
+  }
     //find(".smilie_group").
     //this.innerHTML = e.dataTransfer.getData('text/html');
   }
@@ -115,6 +151,13 @@ $(".smilie").each(function(index, image) {
 	var att=document.createAttribute("draggable");
 	att.value="true";
 	this.setAttributeNode(att);;
+
+  var att1=document.createAttribute("id");
+  console.log(this.getAttribute("id"));
+  if (this.getAttribute("id") != "favourite"){
+    att1.value="basic";
+    this.setAttributeNode(att1);;
+  }
 	});
 
 $(".mainbodytextsmall").each(function(index, image) {
@@ -136,6 +179,7 @@ $(".post").each(function(index, image) {
 	var text = $(this).find(" td.postbody")[0].innerText;
 
 	var author = $(this).find(" dt.author")[0].innerText;
+    var x = new RegExp("(http|https)://(vine)\.(co)/v/[A-Za-z0-9]+");
 	console.log(author);
 	console.log(localStorage.user);
 	
@@ -149,38 +193,32 @@ $(".post").each(function(index, image) {
     console.log(image);
     if (author == localStorage.user){
     $(this).find(" ul.postbuttons").append('<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="manas" data-text="'+text+'" data-count="none" data-dnt="true">Tweet</a></li>');
- 	}
 
- });
 
-$(".post").each(function(index, image) { //vine embed
-	$this = $(image);
-	//var text = this.rows[0].innerText;
-	var text = $(this).find(" td.postbody")[0].innerText;
-	
- // $this = $(this.rows);
-
- 
-
- 	var x = new RegExp("(http|https)://(vine)\.(co)/v/[A-Za-z0-9]+");
-
-	//console.log(x.test("https://vine.co/v/h3Wm0PaxEpM someword"));
-	//var url = x.exec("https://vine.co/v/h3Wm0PaxEpM someword");
-	//console.log(url);
-      //console.log(this.length);
-      //console.log("owned async");
-      text = text.replace(/\"/g,"&quot;");
-      if (x.test(text)){
-      	var url2 = x.exec(text);
+          if (x.test(text)){
+        var url2 = x.exec(text);
        $(this).find(" td.postbody")[0].innerText.replace('/\b('+url2[0]+')\b/g', "dsdfsdfsdfsdfs");
        //console.log($(this).find(" td.postbody")[0].innerText);
        $(this).find(" td.postbody").append('<iframe class="vine-embed" src="'+url2[0]+'/embed/simple" width="600" height="600" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>');
    }
+ 	}
+  
+ // $this = $(this.rows);
+
+ 
+
+
+
+  //console.log(x.test("https://vine.co/v/h3Wm0PaxEpM someword"));
+  //var url = x.exec("https://vine.co/v/h3Wm0PaxEpM someword");
+  //console.log(url);
+      //console.log(this.length);
+      //console.log("owned async");
+
       //console.log(text);
      //console.log(image);
-    
- });
 
+ });
 
 
 });
